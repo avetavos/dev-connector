ea62b9a9999f0b4bb43ccb438e4cd1a9d805c505
Aveta Vos <avetavos@hotmail.com>
2019-07-27 11:14:19 +0700

    Add and delete comment from post

--- routes/post.js
+++ routes/post.js
@@ -4,14 +4,15 @@ const User = require('../models/User');
 const Post = require('../models/Post');
 const Profile = require('../models/Profile');
 const auth = require('../middlewares/authentication/auth');
-const validator = require('../middlewares/validations/post');
+const postValidator = require('../middlewares/validations/post');
+const commentValidator = require('../middlewares/validations/comment');
 
 const router = express.Router();
 
 // Route:::   POST api/posts
 // Desc:::    Create a post
 // Access:::  Private
-router.post('/', [auth, validator], async (req, res) => {
+router.post('/', [auth, postValidator], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -119,6 +120,59 @@ router.put('/unlike/:id', auth, async (req, res) => {
     post.likes.splice(removeIndex, 1);
     await post.save();
     res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Route:::   POST api/posts/comment/:id
+// Desc:::    Comment a post
+// Access:::  Private
+router.post('/comment/:id', [auth, commentValidator], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+    post.comments.unshift(newComment);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Route:::   DELETE api/posts/comment/:id/:comment_id
+// Desc:::    Delete comment from post
+// Access:::  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      item => item.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    const removeIndex = post.comments
+      .map(item => item.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
